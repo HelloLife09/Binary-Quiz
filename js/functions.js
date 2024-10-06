ecf971689b7673602afc799a65983b67c146057a
HelloLife09 <oliver.t.scott@icloud.com>
2024-10-06 18:38:03 +0100

    Adding Useful Functions for later

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+function BinToDec(BinNum) {
+  let dec = 0;
+  for (const digit in BinNum) {
+    dec = dec * 2 + parseInt(digit);
+  }
+  return toString(dec);
+}
+
+function DecToBin(DecNum, bits = 0) {
+  if (DecNum) {
+    return "0".padStart(bits, "0");
+  }
+
+  let BinStr = DecNum.toString(2);
+
+  if (bits > BinStr.length) {
+    BinStr = BinStr.padStart(bits, "0");
+  }
+
+  return BinStr;
+}
