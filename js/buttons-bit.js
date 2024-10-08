46096eff8d603a24045e20b6889972648359412f
HelloLife09 <oliver.t.scott@icloud.com>
2024-10-08 19:29:07 +0100

    Adding button-bit.js elements variables

---
+++ js/buttons-bit.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const BinDisplay = document.getElementById("original-bin");
+  const NumDisplay = document.getElementById("number-display");
+  const SubmitBtn = document.getElementById("number-submit-button");
+  const BtnZero = document.getElementById("zero");
+  const BtnOne = document.getElementById("one");
+  const BtnTwo = document.getElementById("two");
+  const BtnThree = document.getElementById("three");
+  const BtnFour = document.getElementById("four");
+  const BtnFive = document.getElementById("five");
+  const BtnSix = document.getElementById("six");
+  const BtnSeven = document.getElementById("seven");
+  const BtnEight = document.getElementById("eight");
+  const BtnNine = document.getElementById("nine");
+  const BtnBack = document.getElementById("back");
+});
