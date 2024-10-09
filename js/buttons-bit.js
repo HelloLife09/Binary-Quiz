903ba06be6cc7a075360a575c8ce97d0e71abb26
HelloLife09 <160416648+HelloLife09@users.noreply.github.com>
2024-10-09 10:33:43 +0100

    button javascript

--- js/buttons-bit.js
+++ js/buttons-bit.js
@@ -12,5 +12,35 @@ document.addEventListener("DOMContentLoaded", () => {
   const BtnSeven = document.getElementById("seven");
   const BtnEight = document.getElementById("eight");
   const BtnNine = document.getElementById("nine");
+  const NumBtns = [
+    BtnZero,
+    BtnOne,
+    BtnTwo,
+    BtnThree,
+    BtnFour,
+    BtnFive,
+    BtnSix,
+    BtnSeven,
+    BtnEight,
+    BtnNine,
+  ];
   const BtnBack = document.getElementById("back");
+
+  function Submit() {
+    DisplayText = NumDisplay.textContent.trim();
+    if (BinToDec(BinDisplay.textContent) === DisplayText) {
+      BinDisplay.classList.toggle(".correct");
+      NumDisplay.classList.toggle(".correct");
+    }
+  }
+
+  NumBtns.forEach((NumBtn) => {
+    NumBtn.addEventListener("click", () => {
+      NumDisplay.textContent += NumBtn.innerText.trim();
+    });
+  });
+
+  BtnBack.addEventListener("click", () => {
+    NumDisplay.textContent -= 1;
+  });
 });
