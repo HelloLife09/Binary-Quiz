85c156c0a4525f44ad35270278170ab2fc08111d
HelloLife09 <oliver.t.scott@icloud.com>
2024-10-18 11:02:22 +0100

    Added buttons-dec.js for Dec-To-Bin button functionality
    - Fixed DecToBin() in functions.js
    - Added GenerateRandomBin() & GenerateRandomNum()
    - made SubmitButton() function in button.js replacing old SubmitBtn.addEventListener
    - also moved everying outside the DOMContentLoaded event listener
    - added onclick="SubmitButton()" for submit button
    - renamed styles.css to binary-decimal.css
    - moved style.css from main folder to css folder

---
+++ js/buttons-dec.js
@@ -0,0 +1,34 @@
+const NumDisplay = document.getElementById("original-num");
+const BinDisplay = document.getElementById("binary-display");
+const SubmitBtn = document.getElementById("binary-submit-btn");
+const BtnZero = document.getElementById("zero");
+const BtnOne = document.getElementById("one");
+const BtnBack = document.getElementById("back");
+
+function SubmitButton() {
+  const DisplayText = BinDisplay.value.trim();
+  const OriginalNum = DecToBin(NumDisplay.textContent, 5);
+  if (OriginalNum === DisplayText) {
+    BinDisplay.classList.add("correct");
+    NumDisplay.classList.add("correct");
+  } else {
+    BinDisplay.classList.add("incorrect");
+    NumDisplay.classList.add("incorrect");
+    setTimeout(() => {
+      BinDisplay.classList.remove("incorrect");
+      NumDisplay.classList.remove("incorrect");
+    }, 1000);
+  }
+}
+
+BtnZero.addEventListener("click", () => {
+  BinDisplay.value += BtnZero.innerText.trim();
+});
+
+BtnOne.addEventListener("click", () => {
+  BinDisplay.value += BtnOne.innerText.trim();
+});
+
+BtnBack.addEventListener("click", () => {
+  BinDisplay.value = BinDisplay.value.slice(0, -1);
+});
